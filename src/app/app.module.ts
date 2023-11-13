5741557389a3dc7a37e1cfb1d40662b00e99fcd3
vcpersike <vcpersike@gmail.com>
2023-11-13 19:20:24 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 
@@ -9,12 +8,12 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LandingComponent } from './landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
-import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 import { HomeModule } from './home/home.module';
 import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -27,8 +26,9 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    NgbModule,
+    NgModule,
     FormsModule,
     RouterModule,
     AppRoutingModule,
